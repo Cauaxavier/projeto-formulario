de277bab2f38a220f380de20a2987d220a051c63
Cauaxavier <cauagomesxavier5@gmail.com>
2022-10-04 13:54:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projetos
+ projetos de desenvolvimento web.
---
+++ projeto-formulario/inter.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll('.input');
+
+const handlefocus = ({ target }) => {
+    const span = target.previousElementSibling;
+    span.classList.add('span-active');
+}
+
+const handlefocusOut = ({ target }) => {
+    if(target.value == '') {
+        const span = target.previousElementSibling;
+        span.classList.remove('span-active');
+    }
+}
+
+inputs.forEach((input) => input.addEventListener('focus', handlefocus));
+inputs.forEach((input) => input.addEventListener('focusout', handlefocusOut));
